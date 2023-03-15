b3260e7db2e55bde154a64cc80fc801f55c551f9
Tosin2289 <ayoolatosin00@gmail.com>
2023-03-15 19:33:57 +0100

    second commit

---
+++ lib/pages/generator.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Generator extends StatelessWidget {
+  const Generator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
+  }
+}
