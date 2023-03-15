4cc5c13383dd699d896eacddc9d727738ee65919
Tosin2289 <ayoolatosin00@gmail.com>
2023-03-15 21:02:51 +0100

    third commit

--- lib/pages/generator.dart
+++ lib/pages/generator.dart
@@ -1,12 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
-class Generator extends StatelessWidget {
+class Generator extends StatefulWidget {
   const Generator({Key? key}) : super(key: key);
 
   @override
+  State<Generator> createState() => _GeneratorState();
+}
+
+class _GeneratorState extends State<Generator> {
+  String data = '';
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    return Scaffold(
+      backgroundColor: Colors.grey[600],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: QrImage(
+              data: data,
+              backgroundColor: Colors.white,
+              size: 300,
+            ),
+          ),
+          SizedBox(
+            height: 24,
+          ),
+          SizedBox(
+            width: 300,
+            child: TextField(
+              onChanged: ((value) {
+                setState(() {
+                  data = value;
+                });
+              }),
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  hintText: 'Type the Data',
+                  filled: true,
+                  fillColor: Colors.grey[300]),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+        ],
+      ),
     );
   }
 }
